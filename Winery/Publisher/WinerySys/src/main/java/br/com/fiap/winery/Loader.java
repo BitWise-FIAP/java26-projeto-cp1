128f9538bc3fbe195ff16a3ef008339254d70808
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-19 11:33:02 -0300

    wine warning publisher

--- Winery/Publisher/WinerySys/src/main/java/br/com/fiap/winery/Loader.java
+++ Winery/Publisher/WinerySys/src/main/java/br/com/fiap/winery/Loader.java
@@ -5,9 +5,13 @@ import jakarta.xml.ws.Endpoint;
 public class Loader {
     public static void main(String[] args) {
         WineStockServiceImplementation wineStock = new WineStockServiceImplementation();
-
         Endpoint.publish("http://localhost:8085/WineStockService", wineStock);
 
-        System.out.println("Serviço publicado");
+        System.out.println("Serviço WineStock publicado");
+
+        WineWarninServiceImplementation wineWarning = new WineWarninServiceImplementation();
+        Endpoint.publish("http://localhost:8085/WineWarningService", wineWarning);
+
+        System.out.println("Serviço WineWarning publicado");
     }
 }
