1eb6aca240ec013c91485ba989724f4c5d47b11b
Pedro Ferrari <pedrogasparff@gmail.com>
2026-03-19 11:46:17 -0300

    uptades a beca

--- Winery/Consumer/WineOrderClient/src/main/java/br/com/fiap/winery/ApplicationClient2.java
+++ Winery/Consumer/WineOrderClient/src/main/java/br/com/fiap/winery/ApplicationClient2.java
@@ -1,6 +1,7 @@
 package br.com.fiap.winery;
 
 import br.com.fiap.winery.stubclasses.WineStockService;
+import br.com.fiap.winery.stubclasses.WineWarningService;
 import jakarta.xml.ws.Service;
 
 import javax.xml.namespace.QName;
@@ -18,7 +19,20 @@ public class ApplicationClient2 {
         Service service = Service.create(url,qName);
 
         WineStockService wineStockService = service.getPort(WineStockService.class);
-        String menu = wineStockService.getMenu();
-        System.out.println(menu);
+        String order = wineStockService.placeOrder("Syrah", 2);
+        System.out.println(order);
+
+        final String WSDL2 = "http://localhost:8085/WineWarningService?wsdl";
+        final String TARGET2 = "http://winery.fiap.com.br/";
+        final String NAME2 = "WineWarninServiceImplementationService";
+
+        URL url2 = new URL(WSDL2);
+        QName qName2 = new QName(TARGET2, NAME2);
+        Service service2 = Service.create(url2,qName2);
+
+        WineWarningService wineWarningService = service2.getPort(WineWarningService.class);
+
+        String warn = wineWarningService.sendWarn();
+        System.out.println(warn);
     }
 }
